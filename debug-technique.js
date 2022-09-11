a54162496408e477afe7ec35f792231ec252b7df
mohammedtakbir <takbirhossain678@gmail.com>
2022-09-11 23:12:08 +0600

    first commit

---
+++ debug-technique.js
@@ -0,0 +1,11 @@
+// * Debug steps
+/* 
+?   1. Error check (error reproduce)
+?   2. check other stuffs on the website
+?   3. check console fot error
+?   4. click on the link of the error (it will take you to the code)
+?   5. if needed add a breakpoint and try to stop the code over there and see the values
+?   5. if needed console log output 
+?   6. search full code base (crrl+shift+f)-->don't forget about partial match 
+?   7. look around for typo 
+*/
